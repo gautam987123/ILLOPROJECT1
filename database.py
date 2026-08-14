571dcdc7ac7032a6252fd2653a97ef52be5ac436
gautam987123 <gautam06.anand@gmail.com>
2026-08-14 11:09:49 +0530

    Fix database connection function

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import mysql.connector
 
 BASE_DIR = Path(__file__).resolve().parent
 ENV_FILE = BASE_DIR / ".env"
@@ -14,17 +15,20 @@ print("DB_HOST:", repr(os.getenv("DB_HOST")))
 print("DB_USER:", repr(os.getenv("DB_USER")))
 print("DB_NAME:", repr(os.getenv("DB_NAME")))
 
-if __name__ == "__main__":
-    import mysql.connector
 
+def get_connection():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", 3306)),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
+
+
+if __name__ == "__main__":
     try:
-        db = mysql.connector.connect(
-            host=os.getenv("DB_HOST"),
-            port=int(os.getenv("DB_PORT")),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            database=os.getenv("DB_NAME")
-        )
+        db = get_connection()
 
         if db.is_connected():
             print("✅ MySQL database connected successfully!")
